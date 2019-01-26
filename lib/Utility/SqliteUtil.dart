33e6ea7303a65f629dad50e37f9bbb3cfad4c5f5
LKM <cnming2@qq.com>
2019-01-26 17:39:32 +0800

    增加插入,建表,删表方法

--- lib/Utility/SqliteUtil.dart
+++ lib/Utility/SqliteUtil.dart
@@ -1,16 +1,17 @@
-import 'package:glutassistant/Common/Constant.dart';
 import 'package:sqflite/sqflite.dart';
 
-class SqliteUtil {
+import 'package:glutassistant/Common/Constant.dart';
+
+class SQLiteUtil {
   static final int _dbVersion = Constant.DB_VERSION;
   static final String _dbFileName = Constant.File_DB;
   static final String _dbTableName = Constant.VAR_TABLE_NAME;
   static String _dbPath;
-  static var _db;
+  static Database _db;
   static init() async {
     _dbPath = await getDatabasesPath();
     _dbPath = _dbPath + '/' + _dbFileName;
-    _db = openDatabase(_dbPath, version: _dbVersion,
+    _db = await openDatabase(_dbPath, version: _dbVersion,
         onCreate: (Database db, int version) async {
       await db.execute(Constant.SQL_CREATE_TABLE);
     });
@@ -27,8 +28,15 @@ class SqliteUtil {
     _db = null;
   }
 
-  static insertTimetable(Map coursedetail)
-  {
+  static insertTimetable(Map coursedetail) {
+    return _db.insert(_dbTableName, coursedetail);
+  }
 
+  static Future createTable() async {
+    await _db.execute(Constant.SQL_CREATE_TABLE);
+  }
+
+  static Future dropTable() async {
+    await _db.execute(Constant.SQL_DROP_TABLE);
   }
 }
