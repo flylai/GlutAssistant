65ded1fca5e1a7388b3dcb62903100bf163c62fb
LKM <cnming2@qq.com>
2019-02-03 17:46:42 +0800

    添加日期转数字函数

--- lib/Utility/BaseFunctionUtil.dart
+++ lib/Utility/BaseFunctionUtil.dart
@@ -1,5 +1,5 @@
 class BaseFunctionUtil {
-  static int getNumByWeekDay(String weekday) {
+  static int getNumByWeekday(String weekday) {
     switch (weekday) {
       case '星期一':
         return 1;
@@ -19,5 +19,25 @@ class BaseFunctionUtil {
         return 7;
     }
     return 0;
+  }
+
+  static String getWeekdayByNum(int weekday) {
+    switch (weekday) {
+      case 1:
+        return '周一';
+      case 2:
+        return '周二';
+      case 3:
+        return '周三';
+      case 4:
+        return '周四';
+      case 5:
+        return '周五';
+      case 6:
+        return '周六';
+      case 7:
+        return '周日';
+    }
+    return '周一';
   }
 }
