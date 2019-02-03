c6b20c3e47a68275185d54a061792b458af2f568
LKM <cnming2@qq.com>
2019-02-03 17:55:03 +0800

    删除debug变量

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:glutassistant/Pages/Home.dart';
-import 'package:flutter/rendering.dart';
 
 void main() {
-  // debugPaintSizeEnabled = true;
   runApp(MaterialApp(
     home: Home(),
   ));
