b210bfb1b6a93a42af1a79a7c75c59343e2f2b74
LKM <cnming2@qq.com>
2019-02-06 13:27:20 +0800

    添加没有课的提示

--- lib/Pages/Dashboard.dart
+++ lib/Pages/Dashboard.dart
@@ -95,87 +95,103 @@ class _DashboardState extends State<Dashboard> {
 
   List<Widget> _getCourseList() {
     List<Widget> todayCourseList = [];
-    for (int i = 0; i < _courseList.length; i++) {
-      int startTime = _courseList[i]['startTime'];
-      int endTime = _courseList[i]['endTime'];
-      String startTimeStr = startTime.toString();
-      String endTimeStr = endTime.toString();
-      if (startTime > 4) {
-        if (startTime >= 5 && startTime <= 6)
-          startTimeStr = '中午' + (startTime - 4).toString();
-        else
-          startTimeStr = (startTime - 2).toString();
-      }
-      if (endTime > 4) {
-        if (endTime >= 5 && endTime <= 6)
-          endTimeStr = '中午' + (endTime - 4).toString();
-        else
-          endTimeStr = (endTime - 2).toString();
-      }
-      Card course = Card(
-        color: Colors.white.withOpacity(0.7),
-        elevation: 4.0,
-        child: Container(
-          margin: EdgeInsets.all(20),
-          height: 60,
-          width: double.infinity,
-          child: Row(
-            children: <Widget>[
-              Container(
-                  padding: EdgeInsets.fromLTRB(1, 0, 10, 0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.access_time,
-                        color: Color(0xff4090f7),
-                        size: 25,
-                      ),
-                      Text(
-                        '$startTimeStr - $endTimeStr节',
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ],
-                  )),
-              Expanded(
-                  child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    child: Row(children: <Widget>[
-                      Icon(
-                        Icons.class_,
-                        color: Color(0xff4090f7),
-                        size: 25,
-                      ),
-                      Expanded(
-                          child: Text(
-                        _courseList[i]['course'],
-                        style: TextStyle(fontSize: 20),
-                        overflow: TextOverflow.ellipsis,
-                      ))
-                    ]),
-                  ),
-                  Container(
-                    child: Row(children: <Widget>[
-                      Icon(
-                        Icons.location_on,
-                        color: Color(0xff4090f7),
-                        size: 25,
-                      ),
-                      Text(
-                        _courseList[i]['location'],
-                        style: TextStyle(fontSize: 20),
-                      )
-                    ]),
-                  )
-                ],
-              ))
-            ],
+    if (_courseList.length > 0) {
+      for (int i = 0; i < _courseList.length; i++) {
+        int startTime = _courseList[i]['startTime'];
+        int endTime = _courseList[i]['endTime'];
+        String startTimeStr = startTime.toString();
+        String endTimeStr = endTime.toString();
+        if (startTime > 4) {
+          if (startTime >= 5 && startTime <= 6)
+            startTimeStr = '中午' + (startTime - 4).toString();
+          else
+            startTimeStr = (startTime - 2).toString();
+        }
+        if (endTime > 4) {
+          if (endTime >= 5 && endTime <= 6)
+            endTimeStr = '中午' + (endTime - 4).toString();
+          else
+            endTimeStr = (endTime - 2).toString();
+        }
+        Card course = Card(
+          color: Colors.white.withOpacity(0.7),
+          elevation: 4.0,
+          child: Container(
+            margin: EdgeInsets.all(20),
+            height: 60,
+            width: double.infinity,
+            child: Row(
+              children: <Widget>[
+                Container(
+                    padding: EdgeInsets.fromLTRB(1, 0, 10, 0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.access_time,
+                          color: Color(0xff4090f7),
+                          size: 25,
+                        ),
+                        Text(
+                          '$startTimeStr - $endTimeStr节',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ],
+                    )),
+                Expanded(
+                    child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      child: Row(children: <Widget>[
+                        Icon(
+                          Icons.class_,
+                          color: Color(0xff4090f7),
+                          size: 25,
+                        ),
+                        Expanded(
+                            child: Text(
+                          _courseList[i]['course'],
+                          style: TextStyle(fontSize: 20),
+                          overflow: TextOverflow.ellipsis,
+                        ))
+                      ]),
+                    ),
+                    Container(
+                      child: Row(children: <Widget>[
+                        Icon(
+                          Icons.location_on,
+                          color: Color(0xff4090f7),
+                          size: 25,
+                        ),
+                        Text(
+                          _courseList[i]['location'],
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ]),
+                    )
+                  ],
+                ))
+              ],
+            ),
           ),
-        ),
-      );
-      todayCourseList.add(course);
+        );
+        todayCourseList.add(course);
+      }
+    } else {
+      todayCourseList.add(Card(
+          color: Colors.white.withOpacity(0.7),
+          elevation: 4.0,
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.all(20),
+            height: 60,
+            width: double.infinity,
+            child: Text(
+              '今天没有课上哦(๑˙ー˙๑)',
+              style: TextStyle(fontSize: 20),
+            ),
+          )));
     }
     return todayCourseList;
   }
