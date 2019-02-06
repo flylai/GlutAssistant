72e2345cf890266d1cd868a183ca740327a07022
LKM <cnming2@qq.com>
2019-02-06 10:29:01 +0800

    状态栏沉浸

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:glutassistant/Pages/Home.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
+  SystemUiOverlayStyle systemUiOverlayStyle =
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+  SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
 }
