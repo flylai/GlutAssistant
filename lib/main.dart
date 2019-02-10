f2e6ae723901aadf09d6f5b31cf4400ec670fd30
LKM <cnming2@qq.com>
2019-02-10 18:17:39 +0800

    添加抽屉图片, 修改描述文字

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:glutassistant/Pages/Home.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:glutassistant/Redux/State.dart';
+import 'package:glutassistant/Common/Constant.dart';
+import 'package:redux/redux.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(GlutAssistant());
   SystemUiOverlayStyle systemUiOverlayStyle =
       SystemUiOverlayStyle(statusBarColor: Colors.transparent);
   SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+}
+
+class GlutAssistant extends StatelessWidget {
+  final store = Store<GlobalState>(appReducer,
+      initialState: GlobalState(
+        color: Constant.THEME_LIST_COLOR[0][1],
+      ));
+
+  GlutAssistant({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider(
+      store: store,
+      child: StoreBuilder<GlobalState>(builder: (context, store) {
+        return MaterialApp(
+          title: '桂工助手',
+          theme: ThemeData(primaryColor: store.state.color),
+          home: Home(),
+        );
+      }),
+    );
+  }
 }
