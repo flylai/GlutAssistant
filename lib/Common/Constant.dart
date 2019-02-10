44e92b967621413a71f6a1a350836ab7134f26e7
LKM <cnming2@qq.com>
2019-02-10 12:41:45 +0800

    修改一些常量

--- lib/Common/Constant.dart
+++ lib/Common/Constant.dart
@@ -10,7 +10,7 @@ class Constant {
   static final String VAR_TABLE_NAME = "classSchedule";
   static final int VAR_HTTP_TIMEOUT_MS = 6000;
   static final double VAR_COURSE_HEIGHT = 63.0;
-  static final double VAR_DEFAULT_OPACITY = 1.0;
+  static final double VAR_DEFAULT_OPACITY = 0.7;
   static final List<int> VAR_COLOR = [
     0x5CB3CC,
     0x4F383E,
@@ -55,29 +55,45 @@ class Constant {
   static final String URL_CAIWU_INTERFACE =
       "http://cwjf.glut.edu.cn/interface/index";
 
-  static final DRAWER_MAIN_LIST_TITLE = [
-    '常用',
-    '一览',
-    '课程表',
-    '查成绩',
-    '查考试地点',
-    '导入课表',
-    '登录教务',
+
+  static final DRAWER_GENERAL_LIST = [
+    ['常用', Icons.book],
+    ['一览', Icons.dashboard],
+    ['课程表', Icons.date_range],
+    ['查成绩', Icons.photo_library],
+    ['查考试地点', Icons.location_on],
+    ['导入课表', Icons.import_export],
+    ['登录教务', Icons.slideshow]
   ];
-  static final DRAWER_OTHER_LIST_TITLE = ['其他', '设置', '分享', '关于'];
-  static final DRAWER_LIST_TITLE =
-      DRAWER_MAIN_LIST_TITLE + DRAWER_OTHER_LIST_TITLE;
-  static final DRAWER_LIST_ICON = [
-    Icons.book,
-    Icons.dashboard, //总览
-    Icons.date_range, //课程表
-    Icons.photo_library, //成绩
-    Icons.location_on, //考试地点
-    Icons.import_export, //导入课表
-    Icons.slideshow, //教务登录
-    Icons.book,
-    Icons.build, //设置
-    Icons.share, //分享
-    Icons.label //关于
+  static final DRAWER_OTHER_LIST = [
+    ['其他', Icons.book],
+    ['设置', Icons.build],
+    ['分享', Icons.share],
+    ['关于', Icons.label],
+  ];
+  static final DRAWER_LIST = DRAWER_GENERAL_LIST + DRAWER_OTHER_LIST;
+  static final THEME_LIST_COLOR = [
+    ['什么黑', Colors.black],
+    ['姨妈红', Colors.red],
+    ['蓝绿色', Colors.teal],
+    ['少女粉', Colors.pink],
+    ['琥珀黄', Colors.amber],
+    ['橘子黄', Colors.orange],
+    ['原谅绿', Colors.green],
+    ['大海蓝', Colors.blue],
+    ['天空蓝', Colors.lightBlue],
+    ['基佬紫', Colors.purple],
+    ['桂工紫', Color(0xff1a1c81)],
+    ['水绿色', Colors.cyan],
+    ['马鞍棕', Colors.brown],
+    ['石板灰', Colors.grey],
+    ['火药蓝', Colors.blueGrey]
+  ];
+  static final SETTING_LIST_TITLE = [
+    '学号',
+    '教务密码',
+    '当前周',
+    '使用背景图',
+    '选择背景图',
   ];
 }
