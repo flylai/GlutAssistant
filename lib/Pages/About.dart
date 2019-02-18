8972034b18f0629b5d59a811e69487fe6aae08f7
LKM <cnming2@qq.com>
2019-02-18 13:06:08 +0800

    代码格式化

--- lib/Pages/About.dart
+++ lib/Pages/About.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:glutassistant/Common/Constant.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatelessWidget {
-  checkUpdateByCoolapk() async {
-    const url = 'https://www.coolapk.com/apk/com.lkm.glutassistant';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -66,5 +57,14 @@ class About extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  checkUpdateByCoolapk() async {
+    const url = 'https://www.coolapk.com/apk/com.lkm.glutassistant';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
