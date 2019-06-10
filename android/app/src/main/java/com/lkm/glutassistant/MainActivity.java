500710950d230fcd747095be2ef895e9df3ed421
LKM <cnming2@qq.com>
2019-06-10 17:41:21 +0800

    app启动时更新小部件

--- android/app/src/main/java/com/lkm/glutassistant/MainActivity.java
+++ android/app/src/main/java/com/lkm/glutassistant/MainActivity.java
@@ -1,13 +1,21 @@
 package com.lkm.glutassistant;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.lkm.glutassistant.widget.TodayWidgetProvider;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Context context = getApplicationContext();
+        Intent refreshIntent = new Intent(context, TodayWidgetProvider.class).setAction("com.lkm.glutassistant.action.REFRESH");
+        context.sendBroadcast(refreshIntent);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
 }
