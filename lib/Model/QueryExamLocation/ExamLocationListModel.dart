e2a80fa2b600d5df90c09212beab80cfa733ea28
LKM <cnming2@qq.com>
2019-07-18 15:11:18 +0800

    重构考试地点查询

---
+++ lib/Model/QueryExamLocation/ExamLocationListModel.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/foundation.dart';
+import 'package:glutassistant/Common/Constant.dart';
+import 'package:glutassistant/Utility/FileUtil2.dart';
+import 'package:glutassistant/Utility/HttpUtil.dart';
+
+class ExamLocation with ChangeNotifier {
+  bool _isLoading = false;
+  bool _status = false;
+  String _msg = '';
+  List<Map<String, dynamic>> _examList = [];
+
+  bool get isLoading => _isLoading;
+  bool get status => _status;
+  String get msg => _msg;
+  List<Map<String, dynamic>> get examList => _examList;
+
+  Future queryExamList() async {
+    _isLoading = true;
+    notifyListeners();
+
+    FileUtil fp = await FileUtil.getInstance();
+    String cookie = fp.readFile(Constant.FILE_SESSION);
+    _examList.clear();
+    Map<String, dynamic> result = await HttpUtil().queryExamLocation(cookie);
+    if (!result['success']) {
+      _status = false;
+      _msg = '未查到考试信息, 也许是你没登入教务或者最近没有考试';
+    } else {
+      _status = true;
+      _msg = '查询成功';
+      DateTime now = DateTime.now();
+      List<Map<String, dynamic>> tmpList = result['data'];
+      for (var exam in tmpList) {
+        DateTime examTime = DateTime.parse(exam['datetime']);
+        int days = examTime.difference(now).inDays;
+        int hours = examTime.difference(now).inHours - days * 24;
+        int minutes =
+            examTime.difference(now).inMinutes - days * 24 * 60 - hours * 60;
+        String leftTimeStr = '';
+
+        if (days > 0) leftTimeStr += '$days天';
+        if (days > 0 || (days == 0 && hours > 0)) leftTimeStr += '$hours小时';
+        leftTimeStr += '$minutes分钟';
+        exam['leftTime'] = leftTimeStr;
+        _examList.add(exam);
+      }
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+}
---
+++ lib/View/QueryExamLocation/QueryExamLocation.dart
@@ -0,0 +1,103 @@
+import 'dart:core';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:glutassistant/Common/Constant.dart';
+import 'package:glutassistant/Model/GlobalData.dart';
+import 'package:glutassistant/Model/QueryExamLocation/ExamLocationListModel.dart';
+import 'package:glutassistant/Widget/DetailCard.dart';
+import 'package:glutassistant/Widget/SnackBar.dart';
+import 'package:provider/provider.dart';
+
+class QueryExamLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+        builder: (context) => ExamLocation(), child: _buildBody());
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.fromLTRB(13, 13, 13, 0),
+          width: double.infinity,
+          child: _buildQueryButton(),
+        ),
+        Expanded(
+          child: _buildExamWidgetList(),
+        )
+      ],
+    );
+  }
+
+  Widget _buildExamWidgetList() {
+    return Consumer2<ExamLocation, GlobalData>(
+        builder: (context, examLocation, globalData, _) {
+      if (examLocation.isLoading)
+        return Center(child: CircularProgressIndicator());
+
+      List<Widget> examWidgetList = [];
+      if (examLocation.examList.length <= 0) return Container();
+      for (var exam in examLocation.examList) {
+        Color color = Color(Constant
+            .VAR_COLOR[Random.secure().nextInt(Constant.VAR_COLOR.length)]);
+        Widget child = Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Text('${exam['course']}',
+                  style: TextStyle(color: Colors.white)),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              RichText(
+                  text: TextSpan(
+                      style: TextStyle(color: Colors.white),
+                      children: <TextSpan>[
+                    TextSpan(text: '还有'),
+                    TextSpan(text: '${exam['leftTime']}')
+                  ]))
+            ]),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              child: Text('${exam['datetime']} ${exam['interval']}',
+                  style: TextStyle(color: Colors.white)),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: Text('${exam['type']}',
+                  style: TextStyle(color: Colors.white)),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Text('${exam['location']}',
+                  style: TextStyle(color: Colors.white)),
+            )
+          ],
+        );
+        examWidgetList
+            .add(DetailCard(color, child, opacity: globalData.opacity));
+      }
+      return ListView(children: examWidgetList);
+    });
+  }
+
+  Widget _buildQueryButton() {
+    return Consumer<ExamLocation>(
+        builder: (context, examLocation, _) => RaisedButton(
+            onPressed: () async {
+              await examLocation.queryExamList();
+              CommonSnackBar.buildSnackBar(context, examLocation.msg);
+            },
+            color: Colors.blue,
+            child: Text(
+              '查询考试地点',
+              style: TextStyle(color: Colors.white),
+            )));
+  }
+}
