b7e6533358137d1d3df835449ab0a7b1ae694d01
LKM <cnming2@qq.com>
2019-07-31 12:27:29 +0800

    重构课程表界面

---
+++ lib/Model/Timetable/WeekCoureListModel.dart
@@ -0,0 +1,130 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:glutassistant/Common/Constant.dart';
+import 'package:glutassistant/Utility/BaseFunctionUtil.dart';
+import 'package:glutassistant/Utility/SQLiteUtil2.dart';
+
+class WeekCourseList with ChangeNotifier {
+  List<dynamic> _dateList = [];
+  int _selectedWeek = 1;
+  int _currentWeek = 1;
+
+  set selectedWeek(int selectedWeek) {
+    if (_selectedWeek == selectedWeek) return;
+    _selectedWeek = selectedWeek;
+  }
+
+  set currentWeek(int currentWeek) {
+    if (_currentWeek == currentWeek) return;
+    _currentWeek = currentWeek;
+  }
+
+  List<List<Map<String, dynamic>>> _weekCourse = [[], [], [], [], [], [], []];
+  List<Map<String, dynamic>> _courseList = [];
+
+  List<dynamic> get dateList => _dateList;
+  List<Map<String, dynamic>> get courseList => _courseList;
+  List<List<Map<String, dynamic>>> get weekCourse => _weekCourse;
+
+  void refreshDateList() {
+    _dateList.clear();
+    int weekday = DateTime.now().weekday;
+    DateTime day = DateTime.now()
+        .add(Duration(days: (_selectedWeek - _currentWeek) * 7))
+        .subtract(Duration(days: weekday));
+    String month = '${day.add(Duration(days: weekday)).month}\n月';
+    _dateList.add(month);
+
+    for (int i = 1; i <= 7; i++) {
+      DateTime today = day.add(Duration(days: i));
+      String todayStr =
+          today.day == DateTime.now().day && today.month == DateTime.now().month
+              ? '今天'
+              : today.day.toString();
+      Color todayColor = todayStr == '今天' ? Color(0xffEF3473) : Colors.black;
+      String weekdayStr =
+          '周${BaseFunctionUtil().getWeekdayByNum(i)}\n$todayStr';
+      _dateList.add({'color': todayColor, 'weekday': weekdayStr});
+    }
+    notifyListeners();
+  }
+
+  Future<void> refreshTimetable() async {
+    SQLiteUtil su = await SQLiteUtil.getInstance();
+    if (!su.dbIsOpen()) return;
+
+    for (int i = 0; i < 7; i++) {
+      _weekCourse[i].clear();
+      int count = 1;
+      List<Map<String, dynamic>> queryResult =
+          await su.queryCourse(_selectedWeek, i + 1);
+
+      if (queryResult.length > 0) {
+        for (int j = 0; j < queryResult.length; j++) {
+          int startTime = queryResult[j]['startTime'];
+          int endTime = queryResult[j]['endTime'];
+          if (count < startTime) {
+            double marginTop = 0.5 * (startTime - count - 1);
+            double height = (Constant.VAR_COURSE_HEIGHT * (startTime - count));
+            _weekCourse[i].add({
+              'empty': true,
+              'marginTop': marginTop,
+              'padding': 0.0,
+              'color': Colors.transparent,
+              'text': '',
+              'height': height
+            }); //没到有课的时间段 加个空白填充
+            count += startTime - count;
+          }
+          if (count > startTime) continue;
+          int colorIndex = Random.secure().nextInt(Constant.VAR_COLOR.length);
+          double height =
+              Constant.VAR_COURSE_HEIGHT * (endTime - startTime + 1);
+          String text =
+              '${queryResult[j]['courseName']}@${queryResult[j]['location']}';
+          _weekCourse[i].add({
+            'empty': false,
+            'marginTop': 0.5,
+            'padding': 2.0,
+            'color': Color(Constant.VAR_COLOR[colorIndex]),
+            'text': text,
+            'height': height,
+            'weekday': i,
+            'startTime': startTime,
+            'endTime': endTime
+          });
+          count += endTime - startTime + 1;
+        }
+      } else {
+        _weekCourse[i].add({
+          'empty': true,
+          'marginTop': 0.0,
+          'padding': 0.0,
+          'color': Colors.transparent,
+          'text': '',
+          'height': Constant.VAR_COURSE_HEIGHT
+        }); // 一整天没课 空白填充
+      }
+    }
+    notifyListeners();
+  }
+
+  Future<void> queryCourseList(int weekday, int startTime, int endTime) async {
+    _courseList.clear();
+    SQLiteUtil su = await SQLiteUtil.getInstance();
+    if (!su.dbIsOpen()) return;
+
+    List<Map<String, dynamic>> queryResult = await su.queryCourseByTime(
+        _selectedWeek, weekday + 1, startTime, endTime);
+    for (int i = 0; i < queryResult.length; i++) {
+      _courseList.add({
+        'courseName': queryResult[i]['courseName'],
+        'teacher': queryResult[i]['teacher'],
+        'location': queryResult[i]['location'],
+        'week': '${queryResult[i]['startWeek']} - ${queryResult[i]['endWeek']}周'
+      });
+    }
+  }
+}
---
+++ lib/View/Timetable/Timetable.dart
@@ -0,0 +1,220 @@
+import 'dart:async';
+import 'dart:core';
+
+import 'package:flutter/material.dart';
+import 'package:glutassistant/Common/Constant.dart';
+import 'package:glutassistant/Model/GlobalData.dart';
+import 'package:glutassistant/Model/Timetable/WeekCoureListModel.dart';
+import 'package:glutassistant/Utility/BaseFunctionUtil.dart';
+import 'package:provider/provider.dart';
+
+class Timetable extends StatelessWidget {
+  final int _selectedWeek;
+  final int _currentWeek;
+
+  Timetable(this._selectedWeek, this._currentWeek);
+
+  @override
+  Widget build(BuildContext context) {
+    WeekCourseList wcl = WeekCourseList();
+    wcl.selectedWeek = _selectedWeek;
+    wcl.currentWeek = _currentWeek;
+    wcl.refreshDateList();
+    wcl.refreshTimetable();
+    return ChangeNotifierProvider<WeekCourseList>.value(
+      value: wcl,
+      child: Container(child: _buildBody()),
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        _buildDateList(),
+        Expanded(
+          child: GestureDetector(
+              onHorizontalDragEnd: (value) {
+                // if (value.velocity.pixelsPerSecond.dx > 1000 &&
+                //     widget._selectWeek - 1 > 0)
+                //   widget.callback(widget._selectWeek - 1);
+                // if (value.velocity.pixelsPerSecond.dx < -1000 &&
+                //     widget._selectWeek + 1 < 26)
+                //   widget.callback(widget._selectWeek + 1);
+              },
+              child: ListView(
+                children: <Widget>[
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        child: Consumer<GlobalData>(
+                            builder: (context, globalData, _) => Container(
+                                color: Colors.white
+                                    .withOpacity(globalData.opacity),
+                                child: _buildLeftTimeList())),
+                        flex: 1,
+                      ),
+                      Expanded(
+                        child: _buildTimetable(),
+                        flex: 14,
+                      )
+                    ],
+                  )
+                ],
+              )),
+        )
+      ],
+    );
+  }
+
+  Widget _buildDateList() {
+    return Consumer<GlobalData>(
+        builder: (context, globalData, _) => Container(
+            child: _gernerateDateText(),
+            color: Colors.white.withOpacity(globalData.opacity)));
+  }
+
+  Widget _buildLeftTimeList() {
+    List<Widget> leftTimeList = [];
+    for (int i = 1; i < 15; i++) {
+      Container item = Container(
+        alignment: Alignment.center,
+        height: Constant.VAR_COURSE_HEIGHT,
+        child: Text(
+          BaseFunctionUtil().getTimeByNum(i),
+          textAlign: TextAlign.center,
+        ),
+      );
+      leftTimeList.add(Divider(
+        height: 0.6,
+      ));
+      leftTimeList.add(item);
+    }
+    return Column(children: leftTimeList);
+  }
+
+  Widget _buildTimetable() {
+    return Consumer2<WeekCourseList, GlobalData>(
+        builder: (context, weekCourseList, globalData, _) {
+      List<Widget> timetable = [];
+      for (int i = 0; i < 7; i++) {
+        // 一周七天
+        List<Widget> weekdayCourseList = [];
+        for (int j = 0; j < weekCourseList.weekCourse[i].length; j++) {
+          Widget widget = GestureDetector(
+              onTap: () async {
+                if (weekCourseList.weekCourse[i][j]['empty']) return;
+                await _queryCourseList(
+                    context,
+                    weekCourseList.weekCourse[i][j]['weekday'],
+                    weekCourseList.weekCourse[i][j]['startTime'],
+                    weekCourseList.weekCourse[i][j]['endTime']);
+              },
+              child: Container(
+                padding:
+                    EdgeInsets.all(weekCourseList.weekCourse[i][j]['padding']),
+                margin: EdgeInsets.fromLTRB(0.5,
+                    weekCourseList.weekCourse[i][j]['marginTop'], 0.5, 0.5),
+                color: weekCourseList.weekCourse[i][j]['color'].withOpacity(
+                    weekCourseList.weekCourse[i][j]['empty']
+                        ? 0.0
+                        : globalData.opacity),
+                alignment: Alignment.topLeft,
+                height: weekCourseList.weekCourse[i][j]['height'],
+                child: Text(
+                  '${weekCourseList.weekCourse[i][j]['text']}',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ));
+          weekdayCourseList.add(widget);
+        }
+        timetable.add(Expanded(
+          child: Column(
+            children: weekdayCourseList,
+          ),
+          flex: 2,
+        ));
+      }
+      return Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: timetable,
+      );
+    });
+  }
+
+  Future<void> _queryCourseList(context, weekday, startTime, endTime) async {
+    WeekCourseList weekCourseList = Provider.of<WeekCourseList>(context);
+    await weekCourseList.queryCourseList(weekday, startTime, endTime);
+    List<Widget> courselistwidget = [];
+    for (int i = 0; i < weekCourseList.courseList.length; i++) {
+      Container course = Container(
+        margin: EdgeInsets.fromLTRB(15, 10, 15, 10),
+        padding: EdgeInsets.all(7),
+        decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Icon(Icons.book, color: Colors.amber),
+                Text(weekCourseList.courseList[i]['courseName'])
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.person, color: Colors.pink),
+                Text(weekCourseList.courseList[i]['teacher'])
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.location_on, color: Colors.teal),
+                Text(weekCourseList.courseList[i]['location'])
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Icon(Icons.access_time, color: Colors.cyan),
+                Text('${weekCourseList.courseList[i]['week']}')
+              ],
+            )
+          ],
+        ),
+      );
+      courselistwidget.add(course);
+    }
+    showDialog(
+        context: context,
+        builder: (BuildContext ctx) {
+          return Dialog(
+              child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: courselistwidget,
+          ));
+        });
+  }
+
+  Widget _gernerateDateText() {
+    return Consumer<WeekCourseList>(builder: (context, weekCourseList, _) {
+      List<Widget> datelist = [];
+      Widget month = Expanded(
+        child:
+            Text('${weekCourseList.dateList[0]}', textAlign: TextAlign.center),
+        flex: 1,
+      );
+      datelist.add(month);
+
+      for (int i = 1; i <= 7; i++) {
+        Widget wd = Expanded(
+          child: Text(
+            '${weekCourseList.dateList[i]['weekday']}',
+            style: TextStyle(color: weekCourseList.dateList[i]['color']),
+            textAlign: TextAlign.center,
+          ),
+          flex: 2,
+        );
+        datelist.add(wd);
+      }
+      return Row(children: datelist);
+    });
+  }
+}
