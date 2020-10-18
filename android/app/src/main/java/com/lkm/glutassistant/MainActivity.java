ccd891adae62c60881fdd0c27bac1363bd4c713d
LKM <cnming2@qq.com>
2020-10-18 18:31:54 +0800

    移植到flutter几来着

--- android/app/src/main/java/com/lkm/glutassistant/MainActivity.java
+++ android/app/src/main/java/com/lkm/glutassistant/MainActivity.java
@@ -1,21 +1,32 @@
 package com.lkm.glutassistant;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
+import androidx.annotation.NonNull;
 
-import com.lkm.glutassistant.widget.TodayWidgetProvider;
-
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        Context context = getApplicationContext();
+//        Intent refreshIntent = new Intent(context, TodayWidgetProvider.class).setAction("com.lkm.glutassistant.action.REFRESH");
+//        context.sendBroadcast(refreshIntent);
+//        GeneratedPluginRegistrant.registerWith(this);
+//    }
+
+    private static final String CHANNEL = "samples.flutter.dev/battery";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Context context = getApplicationContext();
-        Intent refreshIntent = new Intent(context, TodayWidgetProvider.class).setAction("com.lkm.glutassistant.action.REFRESH");
-        context.sendBroadcast(refreshIntent);
-        GeneratedPluginRegistrant.registerWith(this);
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // Your existing code
+                        }
+                );
     }
 }
